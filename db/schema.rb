cd6d6fd3a16e7206616bae023b97493b4f4d7ebf
nauosika <nauosikaforwork@gmail.com>
2021-09-24 00:52:26 +0800

    orders add column state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_141704) do
+ActiveRecord::Schema.define(version: 2021_09_23_165112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_23_141704) do
     t.integer "product_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "state"
   end
 
   create_table "products", force: :cascade do |t|
