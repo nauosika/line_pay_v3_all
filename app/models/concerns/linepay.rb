1d393e17a837ce87c4b3b45e09efe68904c70b6a
nauosika <79121183+nauosika@users.noreply.github.com>
2021-09-25 17:35:48 +0800

    Line_pay_api moved to module (#5)

---
+++ app/models/concerns/linepay.rb
@@ -0,0 +1,77 @@
+module Linepay
+  extend ActiveSupport::Concern
+
+  def request_response
+    self.requset_body()
+    self.prepare_require_signature()
+    self.require_header()
+    @uri = URI.parse("https://sandbox-api-pay.line.me/v3/payments/request")
+    self.get_response()
+  end
+
+  def confirm_response(transactionId)
+    self.confirm_body()
+    self.prepare_confirm_signature(transactionId)
+    self.require_header()
+    @uri = URI.parse("https://sandbox-api-pay.line.me" + "#{@post_uri}")
+    self.get_response()
+  end
+
+  private
+  def requset_body
+    @body = { amount: self.amount,
+              currency: self.currency,
+              orderId: self.order_id,
+              packages: [ { id: self.packages_id,
+                            amount: self.amount,
+                            products: [ {
+                            name: self.name,
+                            quantity: self.quantity,
+                            price: self.price } ] } ],
+              redirectUrls: { confirmUrl: "http://127.0.0.1:3000/confirm",
+                              cancelUrl: "http://127.0.0.1:3000/cancel" } }
+  end
+
+  def confirm_body
+    @body = { amount: self.amount,
+              currency: self.currency
+    }
+  end
+
+  def get_nonce_and_secrect
+    @nonce = SecureRandom.uuid
+    @secrect = ENV["lines_pay_ChannelSecret"]
+  end
+
+  def prepare_require_signature
+    get_nonce_and_secrect()
+    @post_uri = "/v3/payments/request"
+  end
+
+  def prepare_confirm_signature(transactionId)
+    get_nonce_and_secrect()
+    @post_uri = "/v3/payments/" + "#{transactionId}" + "/confirm"
+  end
+
+  def get_signature
+    message = "#{@secrect}#{@post_uri}#{@body.to_json}#{@nonce}"
+    hash = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), @secrect, message)
+    @signature = Base64.strict_encode64(hash)
+  end
+
+  def require_header
+    get_signature()
+    @header = {"Content-Type": "application/json",
+          "X-LINE-ChannelId": ENV["line_pay_ChannelID"],
+          "X-LINE-Authorization-Nonce": @nonce,
+          "X-LINE-Authorization": @signature }
+  end
+
+  def get_response
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(@uri.request_uri, @header)
+    request.body = @body.to_json
+    @response = http.request(request)
+  end
+end
