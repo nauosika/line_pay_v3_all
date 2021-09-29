239098bb7e337fff3bae1c726bbb3ebc031a2bde
nauosika <nauosikaforwork@gmail.com>
2021-09-30 00:28:54 +0800

    mail setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "line_pay_api_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'https://nauosika-5229.herokuapp.com/', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'nauosika-line-test.herokuapp.com', :protocol => 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address: "smtp.mailgun.org",
