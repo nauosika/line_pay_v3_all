cd672ec9258323e00b540b29ecd7da5611f338cc
nauosika <nauosikaforwork@gmail.com>
2021-09-30 17:44:13 +0800

    ...

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'sandboxa6d1bd63cec64129a7e566ac54cca4ef.mailgun.org'
+  config.mailer_sender = 'nauosikatest@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
